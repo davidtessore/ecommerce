f46d24b42cfa1b42de1eb4c34e7fc6ee74e42685
davidtessore <davidtessore_1991@hotmail.com>
2022-10-11 17:12:35 -0300

    casi falta configurar los favoritos

--- src/components/productCard/ProductCard.js
+++ src/components/productCard/ProductCard.js
@@ -2,18 +2,24 @@ import "./productCard.scss";
 import { Link } from "react-router-dom";
 
 const ProductCard = ({ data }) => {
-    const { img1, title, price, category, id } = data;
+    console.log(data)
+    const { img1, title, description, price, category, id } = data;
 
     return (
         <div className="card">
-            <Link to={`/${category}/${id}`}>
-                <div className="card__imgContainer">
-                    <img className="card__img" src={`/assets/images/${img1}`} alt="Imagen Producto" />
+            <div className="card__cardContainer">
+                <Link className="card__link" to={`/${category}/${id}`}>
+                    <div className="card__imgContainer">
+                        <img className="card__img" src={`/assets/images/${img1}`} alt="Imagen Producto" />
+                    </div>
+                </Link>
+                <img className="card__icoImg" src="./assets/ico/favourite/favouriteEmpty.png" alt="Icono Favorito" />
+                <hr className="card__line" />
+                <div className="card__contentContainer">
+                    <h3 className="card__title">{title}</h3>
+                    <p className="card__paragraph">{description}</p>
+                    <p className="card__price">Precio: ${price}</p>
                 </div>
-            </Link>
-            <div className="card__contentContainer">
-                <h3 className="card__title">{title}</h3>
-                <span className="card__price">${price}</span>
             </div>
         </div>
     );
